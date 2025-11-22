e802cb21e1d4f287f1f0a97c629bc346e10393c3
Mitchstarc56 <TonyStarc544@gmail.com>
2025-11-22 10:06:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Worker: paste this as your worker entry file
 const STREAMS = {
-  "t1": {
-    mpd: "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd",
-    clearkeys: "d0f2e5c39e70c18f29bf77768a1ad89a:d6853c51fcf37a18905f0609972395d7"
-  },
   "w": {
     mpd: "https://abbbmftaaaaaaaamgdshloqn26zka.otte.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/7tql9fsdjo/out/v1/46b477f97cd742b087401526419edc67/cenc.mpd",
     clearkeys: "8ca5d080818584785c022021b8fe8179:60261514a077c36fa5e5933fb6049880"
@@ -15,7 +11,35 @@ const STREAMS = {
   "asc": {
     mpd: "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash-sd/enc/unphwrnhdm/out/v1/b099c32d36304e01834cc9df1b600a30/cenc-sd.mpd",
     clearkeys: "7478fc940c1a5f18056cd8dd1e32786c:54aeadaa13fa9ef051d68990894ceec4"
-  }
+  },
+  "t1": {
+    mpd: "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd",
+    clearkeys: "d0f2e5c39e70c18f29bf77768a1ad89a:d6853c51fcf37a18905f0609972395d7"
+  },
+  "t2": {
+    mpd: "https://otte.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/irplqtcjlv/out/v1/f8fa17f087564f51aa4d5c700be43ec4/cenc.mpd",
+    clearkeys: "0d9efa8287a52a2497a281f7a9012211:e4f832d372943014fe5c30928a90e547|"
+  },
+  "t3": {
+    mpd: "https://otte.live.cf.ww.aiv-cdn.net/lhr-nitro/live/dash/enc/lsdasbvglv/out/v1/bb548a3626cd4708afbb94a58d71dce9/cenc.mpd",
+    clearkeys: "a93c1cbfdccd23233bac13540c693e7f:2f6ab2e6693eb847eff3c9da8f9d01fc"
+  },
+  "t4": {
+    mpd: "https://otte.live.cf.ww.aiv-cdn.net/lhr-nitro/live/dash/enc/i2pcjr4pe5/out/v1/912e9db56d75403b8a9ac0a719110f36/cenc.mpd",
+    clearkeys: "192b1115da041585c77200128549efa1:634e10efe4abbb14be400a3ccbac0258"
+  },
+  "ws": {
+    mpd: "https://otte.live.cf.ww.aiv-cdn.net/lhr-nitro//live/clients/dash/enc/f60kqesunw/out/v1/a435ed7a00f947deb4369b46d8f2fb70/cenc.mpd",
+    clearkeys: "1779c27b9d077a3ba0c9cc1bb9a94b9f":"cc5cf3b7928fb9e0a1ee6a8b566f0a8e"
+  },
+  "f1": {
+    mpd: "https://a166aivottlinear-a.akamaihd.net/OTTB/sin-nitro/live/clients/dash/enc/inpyms8ezu/out/v1/1084d5c9a97a4c5b9f9554c88f486646/cenc.mpd",
+    clearkeys: "065051b99bf5cf8d9a3bde5cbde6aaf9:214bd176832872339ce184338320f9a2"
+  },
+  "f2": {
+    mpd: "https://a204aivottepl-a.akamaihd.net/sin-nitro/live/clients/dash/enc/fdb3pubmek/out/v1/aefca6420f944a9482e117f315de535f/cenc.mpd",
+    clearkeys: "7e9239c1982d984a002df3ed049d0756:1b8a17598129a3618535c8fb05f103fe"
+  } 
 };
 
 const ALLOWED_DOMAINS = [
